5ecd969d58933633978c679b445ac9c45b4a8075
Sakura Vera <mayralvm@hotmail.com>
2020-09-06 01:57:00 -0500

    Updates

---
+++ precache-manifest.a8b9d01870083f5ea754c62f93bfa733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ce92122b216f41d9de5390d063cbe7",
+    "url": "/creando/index.html"
+  },
+  {
+    "revision": "de67493ffb198874d193",
+    "url": "/creando/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3373a03ae7613e4cbeac",
+    "url": "/creando/static/js/2.20c39bf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/creando/static/js/2.20c39bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de67493ffb198874d193",
+    "url": "/creando/static/js/main.aceeb583.chunk.js"
+  },
+  {
+    "revision": "a298ffecb0815d748134",
+    "url": "/creando/static/js/runtime-main.5220175f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/creando/static/media/logo.5d5d9eef.svg"
+  }
+]);
